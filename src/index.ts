260d7500a024c39708a3de594fb463f70a0715de
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2020-12-18 22:28:05 +0900

    export

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+
+const generateMarkdown = (json: string): string => {
+
+  return "";
+}
+
+class SwaggerMd {
+  private _object: any;
+
+  constructor(body: string) {
+    this._object = JSON.parse(body);
+  }
+}
+
+export default generateMarkdown;
