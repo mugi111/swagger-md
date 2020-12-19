0081267d56beb4dcc9b1271b3d3c0acfcb938131
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2020-12-20 05:44:57 +0900

    add file exist check

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,19 @@ import { SwaggerMd } from './SwaggerMd';
 const run = (): void => {
   const [ , , jsonPath] = process.argv;
 
-  if (fs.statSync(jsonPath)){
+  if (!isExist(jsonPath)) {
     console.log(chalk.red("not found !!"));
     return;
+  }
+  console.log(chalk.green("Load " + jsonPath));
+}
+
+const isExist = (path: string): boolean => {
+  try {
+    fs.statSync(path);
+    return true;
+  } catch(err) {
+    return false;
   }
 }
 
