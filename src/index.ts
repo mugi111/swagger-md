358eb548f7dbd2df034babfcca84bcfd9d3badc8
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2020-12-20 04:58:10 +0900

    add not found message

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import fs from 'fs';
+import chalk from 'chalk';
 import { SwaggerMd } from './SwaggerMd';
 
 const run = (): void => {
   const [ , , jsonPath] = process.argv;
 
   if (fs.statSync(jsonPath)){
+    console.log(chalk.red("not found !!"));
     return;
   }
 }
