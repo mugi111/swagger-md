adc503712385bbb70f5f211fe39228b4f8bedd38
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2020-12-20 18:02:37 +0900

    Feature read file (#1)
    * lib chalk
    
    * add not found message
    
    * add file exist check
    
    * add example.json
    
    * read file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
 import fs from 'fs';
+import chalk from 'chalk';
 import { SwaggerMd } from './SwaggerMd';
 
 const run = (): void => {
   const [ , , jsonPath] = process.argv;
 
-  if (fs.statSync(jsonPath)){
+  if (!isExist(jsonPath)) {
+    console.log(chalk.red("not found !!"));
     return;
+  }
+
+  const jsonBody = fs.readFileSync(jsonPath, "utf-8");
+  console.log(chalk.green("Load " + jsonPath));
+
+  const swaggerMd = new SwaggerMd(jsonBody);
+}
+
+const isExist = (path: string): boolean => {
+  try {
+    fs.statSync(path);
+    return true;
+  } catch(err) {
+    return false;
   }
 }
 
