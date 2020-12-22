78837d858dd9b067e700c8f709f81c7db7a3d4d5
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2020-12-22 17:37:58 +0900

    remove details

--- src/SwaggerMd.ts
+++ src/SwaggerMd.ts
@@ -20,15 +20,15 @@ export class SwaggerMd {
   private printEndpoint = (obj: any): void => {
     for (const method in obj) {
       const request = obj[method] as Request;
-      this._generated += `<details><summary>${method}</summary>\n`;
+      this._generated += `#### ${method}\n`;
       this._generated += `${request.description}  \n`;
-      this._generated += `#### Parameters  \n`;
+      this._generated += `##### Parameters  \n`;
       this._generated += `| Name | Type | Description |\n`;
       this._generated += `|------|------|-------------|\n`;
       for (const param of request.parameters) {
         this._generated += `| ${param.name} | ${param.type} | ${param.description} |\n`;
       }
-      this._generated += `\n</details>\n`;
+      this._generated += `\n`;
     }
   }
 
