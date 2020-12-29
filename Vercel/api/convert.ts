1268f866de6bf13df0ab49520d3310518d27af46
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2020-12-30 02:16:55 +0900

    0.0.1

--- Vercel/api/convert.ts
+++ Vercel/api/convert.ts
@@ -1,9 +1,10 @@
 import { NowRequest, NowResponse } from '@vercel/node';
+import SwaggerMd from 'swagger-md';
 
 export default (request: NowRequest, response: NowResponse) => {
   if(request.method === 'POST') {
-
-    response.status(200).send(request.body);
+    const converter: SwaggerMd = new SwaggerMd(request.body);
+    response.status(200).send(converter.output());
   } else {
     response.status(405);
   }
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,3 @@
-import fs from 'fs';
-import chalk from 'chalk';
 import { SwaggerMd } from './SwaggerMd';
 
-const run = (): void => {
-  const [ , , jsonPath] = process.argv;
-
-  if (!isExist(jsonPath)) {
-    console.log(chalk.red("not found !!"));
-    return;
-  }
-
-  const jsonBody: string = fs.readFileSync(jsonPath, "utf-8");
-  console.log(chalk.green("Load " + jsonPath));
-
-  const swaggerMd: SwaggerMd = new SwaggerMd(jsonBody);
-  console.log(chalk.green("Load Success !!"));
-  console.log(swaggerMd.output());
-}
-
-const isExist = (path: string): boolean => {
-  try {
-    fs.statSync(path);
-    return true;
-  } catch(err) {
-    return false;
-  }
-}
-
-run();
+export default SwaggerMd;
